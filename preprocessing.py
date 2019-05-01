9ae928b893889eabcaf35c3966128187ba9c6d7c
James Lin <james97lin@gmail.com>
2019-05-01 00:11:36 -0700

    Reworked temporal_sync, finished preprocessing utils.
    Gave up on the idea of maintaining separate fps levels during temporal sync and preprocessing. Instead, both vr and mocap data get upsampled to their fps's LCM and then the two are downsampled from there. This means the original vr input is not kept, but I don't think there's significant value in doing so (50 fps should be high enough that linear interpolation is a reasonable approximation).

--- preprocessing.py
+++ preprocessing.py
@@ -7,19 +7,19 @@ import numpy as np
 
 class VRData:
 
-    def __init__(self, num_frames, time_per_frame, calibration_frame, frames):
+    def __init__(self, num_frames, fps, calibration_frame, frames):
         self.num_frames = num_frames
-        self.time_per_frame = time_per_frame
+        self.fps = fps
         self.calibration_frame = calibration_frame
         self.frames = frames
 
 
 class MocapData:
 
-    def __init__(self, num_frames, time_per_frame, frames):
+    def __init__(self, num_frames, fps, frames):
         # TODO: decide if the skeleton data is required as well
         self.num_frames = num_frames
-        self.time_per_frame = time_per_frame
+        self.fps = fps
         self.frames = frames
 
 
@@ -32,7 +32,7 @@ def load_vr_file(file):
 
     f = open(file, "r")
     num_frames = int(f.readline().split()[1])
-    time_per_frame = float(f.readline().split()[2])
+    fps = int(np.round(1 / float(f.readline().split()[2])))
     calibration_frame = np.array([float(token) for token in f.readline().split()[1:]])
     num_channels = calibration_frame.size
     frames = np.zeros((num_frames, num_channels))
@@ -40,7 +40,7 @@ def load_vr_file(file):
     for i in range(num_frames):
         frames[i] = np.array([float(token) for token in f.readline().split()])
 
-    return VRData(num_frames, time_per_frame, calibration_frame, frames)
+    return VRData(num_frames, fps, calibration_frame, frames)
 
 
 def load_mocap_file_helper(f):
@@ -51,7 +51,7 @@ def load_mocap_file_helper(f):
     """
 
     num_frames = int(f.readline().split()[1])
-    time_per_frame = float(f.readline().split()[2])
+    fps = int(np.round(1 / float(f.readline().split()[2])))
     bookmark = f.tell()
     num_channels = len(f.readline().split())
     f.seek(bookmark)
@@ -60,7 +60,7 @@ def load_mocap_file_helper(f):
     for i in range(num_frames):
         frames[i] = np.array([float(token) for token in f.readline().split()])
 
-    return MocapData(num_frames, time_per_frame, frames)
+    return MocapData(num_frames, fps, frames)
 
 
 def load_mocap_file(file):
@@ -108,3 +108,186 @@ def transform_mocap_frame(frame):
         new_frame[nf_index] = np.sin(frame[f_index])
         new_frame[nf_index+1] = np.cos(frame[f_index])
     return new_frame
+
+
+def preprocess_data(mocap_data, vr_data, offset, desired_fps):
+    """
+    Preprocesses mocap_data and vr_data. Performs the offset and sampling required.
+    :param mocap_data: MocapData object
+    :param vr_data: VRData object (processed)
+    :param offset: temporal offset - move mocap_data forward in time.
+    :param desired_fps: fps each sequence should end up at. assumed to divisble under lcm of fps's
+    :return: None
+    """
+
+    # Upsample both
+    mocap_fps, vr_fps = mocap_data.fps, vr_data.fps
+    fps = np.lcm(mocap_fps, vr_fps)
+    upsample_mocap_vec(mocap_data, fps)
+    upsample_vr_vec(vr_data, fps)
+    mocap_vec, vr_vec = mocap_data.frames, vr_data.frames
+
+    # Apply offset and downsample
+    sample_offset, skip = fps*offset, fps//desired_fps
+    mocap_vec = mocap_vec[max(-sample_offset, 0):]
+    vr_vec = vr_vec[max(sample_offset, 0):]
+    min_length = min(mocap_vec.shape[0], vr_vec.shape[0])
+    mocap_vec = mocap_vec[:min_length:skip]
+    vr_vec = vr_vec[:min_length:skip]
+    mocap_data.num_frames, mocap_data.frames = mocap_vec.shape[0], mocap_vec
+    vr_data.num_frames, vr_data.frames = vr_vec.shape[0], vr_vec
+    vr_data.fps, mocap_data.fps = desired_fps, desired_fps
+
+    # Convert vr inputs to sin/cos
+    convert_vr_vec_to_sin_cos(vr_data)
+
+    # Scale down positions of mocap (centimeters to meters)
+    mocap_data.frames[:, :3] /= 100
+
+
+def convert_vr_vec_to_sin_cos(vr_data):
+    """
+    Converts all angles in vr_data to sin/cos values
+    :param vr_data: VRData object
+    :return: None
+    """
+
+    vr_vec = vr_data.frames
+    n_vr_vec = np.zeros((vr_vec.shape[0], 27))  # 9 (pos) + 9*2 (sin/cos)
+
+    for obj_idx in range(3):
+        i, j = 6*obj_idx, 9*obj_idx
+        n_vr_vec[:, j:j+3] = vr_vec[:, i:i+3]
+        n_vr_vec[:, j+3:j+9:2] = np.sin(vr_vec[:, i+3:i+6])
+        n_vr_vec[:, j+4:j+9:2] = np.cos(vr_vec[:, i+3:i+6])
+
+    vr_data.num_frames = n_vr_vec.shape[0]
+    vr_data.frames = n_vr_vec
+
+
+def upsample_vr_vec(vr_data, fps):
+    """
+    Upsamples the data within vr_data to the desired fps
+    :param vr_data: VRData object (processed)
+    :param fps: desired fps (assumed to be a multiple of vr_fps)
+    :return: None
+    """
+
+    vr_vec, vr_fps = vr_data.frames, vr_data.fps
+    res = fps // vr_fps
+    length = (vr_vec.shape[0]-1)*res + 1
+    n_vr_vec = np.zeros((length, vr_vec.shape[1]))
+
+    for obj_idx in range(3):
+        for subsample in range(res):
+            alpha = subsample / res
+            i = 6*obj_idx
+            pos_x, pos_y = vr_vec[:-1, i:i+3], vr_vec[1:, i:i+3]
+            rot_x, rot_y = vr_vec[:-1, i+3:i+6], vr_vec[1:, i+3:i+6]
+            n_vr_vec[subsample:-1:res, i:i+3] = lerp_position(pos_x, pos_y, alpha)
+            n_vr_vec[subsample:-1:res, i+3:i+6] = lerp_angle(rot_x, rot_y, alpha)
+    n_vr_vec[-1] = vr_vec[-1]
+
+    vr_data.num_frames = n_vr_vec.shape[0]
+    vr_data.frames = n_vr_vec
+    vr_data.fps = fps
+
+
+def upsample_mocap_vec(mocap_data, fps):
+    """
+    Upsamples the data within mocap_data to the desired fps
+    :param mocap_data: MocapData object
+    :param fps: desired fps
+    :return: None
+    """
+
+    mocap_vec, mocap_fps = mocap_data.frames, mocap_data.fps
+    res = fps // mocap_fps
+    length = (mocap_vec.shape[0]-1)*res + 1
+    n_mocap_vec = np.zeros((length, mocap_vec.shape[1]))
+
+    pos_x, pos_y = mocap_vec[:-1, :3], mocap_vec[1:, :3]
+    for subsample in range(res):
+        alpha = subsample / res
+        n_mocap_vec[subsample:-1:res, :3] = lerp_position(pos_x, pos_y, alpha)
+    rot_x, rot_y = mocap_vec[:-1, 3:], mocap_vec[1:, 3:]
+    for subsample in range(res):
+        alpha = subsample / res
+        n_mocap_vec[subsample:-1:res, 3:] = lerp_angle(rot_x, rot_y, alpha)
+    n_mocap_vec[-1] = mocap_vec[-1]
+
+    mocap_data.num_frames = n_mocap_vec.shape[0]
+    mocap_data.frames = n_mocap_vec
+    mocap_data.fps = fps
+
+
+def lerp_position(x, y, alpha):
+    """
+    Lerp between x and y, parameterized by alpha
+    :param x: float or array of such
+    :param y: float or array of such
+    :param alpha: float [0-1]
+    :return: float or array
+    """
+
+    return x + (y - x) * alpha
+
+
+def lerp_angle(x, y, alpha):
+    """
+    Lerp between angles x and y, parameterized by alpha
+    :param x: angle (degrees) or array of such
+    :param y: angle (degrees) or array of such
+    :param alpha: float [0-1]
+    :return: float or array
+    """
+
+    shortest_angle = ((y-x) + 180) % 360 - 180
+    return (x + alpha * shortest_angle) % 360
+
+
+# Testing lerp_angle
+# print(lerp_angle(0, 30, .5))
+# print(lerp_angle(40, 190, 1))
+# print(lerp_angle(0, 180, .75))
+
+# Testing lerp position
+# print(lerp_position(0, 20, .5))
+# print(lerp_position(-23, 7, .25))
+# print(lerp_position(0, 1, 1))
+#print(lerp_position(np.arange(10).reshape((2,5)), np.arange(10).reshape((2, 5)) + 5, .25))
+
+# Testing upsample vr vec
+# vr_data = load_vr_file("data/vr/2019-16-4--16-24-35-processed.txt")
+# upsample_vr_vec(vr_data, 100)
+# print(vr_data.num_frames)
+# print(vr_data.frames.shape)
+# print(vr_data.frames[:10, 2:4])
+
+# Testing upsample mocap vec
+# mocap_data = load_mocap_file("data/mocap/Take 2019-04-16 04.19.31 PM.bvh")
+# upsample_mocap_vec(mocap_data, 360)
+# print(mocap_data.num_frames)
+# print(mocap_data.frames.shape)
+# print(mocap_data.frames[:20,8])
+
+# Testing sin/cos conversion
+# vr_data = load_vr_file("data/vr/2019-16-4--16-24-35-processed.txt")
+# convert_vr_vec_to_sin_cos(vr_data)
+# print(vr_data.num_frames, vr_data.frames.shape)
+# print(vr_data.frames[:20, 9:18])
+
+# Testing preprocess
+# vr_data = load_vr_file("data/vr/2019-16-4--16-24-35-processed.txt")
+# mocap_data = load_mocap_file("data/mocap/Take 2019-04-16 04.19.31 PM.bvh")
+# preprocess_data(mocap_data, vr_data, 1, vr_data.fps)
+# print(vr_data.num_frames, mocap_data.num_frames)
+# print(vr_data.fps, mocap_data.fps)
+# print(vr_data.frames.shape, mocap_data.frames.shape)
+# print(vr_data.frames[0])
+# print(mocap_data.frames[0])
+#
+# import matplotlib.pyplot as plt
+# plt.plot(np.arange(vr_data.num_frames)/vr_data.fps, vr_data.frames[:,1], "r")
+# plt.plot(np.arange(vr_data.num_frames)/vr_data.fps, mocap_data.frames[:,1], "b")
+# plt.show()
---
+++ temporal_sync_new.py
@@ -0,0 +1,126 @@
+import preprocessing
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import getopt
+import sys
+
+vr_file = ""
+mocap_file = ""
+
+
+def get_correlation(x, y, offset):
+    """
+    Finds the correlation between x and y at the given offset.
+    x is shifted forward by the offset.
+    :param x: 1D array of float values
+    :param y: 1D array of float values
+    :param offset: how much to offset x by
+    :param x_skip: take every x_skip samples in x
+    :param y_skip: take every y_skip samples in y
+    :return: float representing degree of correlation
+    """
+
+    x = x[max(-offset, 0):]
+    y = y[max(offset, 0):]
+    min_length = min(x.size, y.size)
+    x = x[:min_length]
+    y = y[:min_length]
+    return np.dot(x, y)
+
+
+def get_temporal_offset(x, y, fps, base, window, start=None, end=None):
+    """
+    Finds best temporal offset aligning x and y.
+    :param x: np array
+    :param y: np array
+    :param fps: sample rate of x and y
+    :param base: seconds; starting rate to search around
+    :param window: seconds; search radius
+    :param start: starting time in x to consider correlation
+    :param end: ending time in x to consider correlation
+    :return: seconds to shift x forward in time
+    """
+
+    start_sample = int(fps * (0 if start is None else start))
+    end_sample = int(fps * (1000000 if end is None else end))
+    x = x[start_sample:end_sample]
+
+    start_sample_offset, end_sample_offset = int(fps*(base-window)), int(fps*(base+window))
+    correlations = np.zeros(end_sample_offset - start_sample_offset + 1)
+    max_sample_offset, max_correlation = None, -np.inf
+
+    for sample_offset in range(start_sample_offset, end_sample_offset+1):
+        correlation = get_correlation(x, y, sample_offset + start_sample)
+        correlations[sample_offset-start_sample_offset] = correlation
+        if correlation > max_correlation:
+            max_sample_offset, max_correlation = sample_offset, correlation
+
+    return max_sample_offset / fps
+
+
+def process_args():
+    options = "v:m:"
+    long_options = ["vr=", "mocap="]
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], options, long_options)
+    except getopt.GetoptError:
+        print("temporal_sync.py -v <vr> -m <mocap>")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-v", "--vr"):
+            global vr_file
+            vr_file = arg
+        elif opt in ("-m", "--mocap"):
+            global mocap_file
+            mocap_file = arg
+
+def main():
+    # Command line arguments
+    process_args()
+
+    # Load vr data
+    vr_data = preprocessing.load_vr_file(vr_file)
+    vr_vec = vr_data.frames[:, 1]
+    vr_fps = vr_data.fps
+    vr_vec = (vr_vec - np.mean(vr_vec)) / np.std(vr_vec)
+
+    # Load mocap data
+    mocap_data = preprocessing.load_mocap_file_helper(open(mocap_file, "r"))
+    mocap_vec = mocap_data.frames[:, 1]
+    mocap_fps = mocap_data.fps
+    mocap_vec = (mocap_vec - np.mean(mocap_vec)) / np.std(mocap_vec)
+
+    # Upsample both sequences
+    fps = np.lcm(vr_fps, mocap_fps)
+    vr_vec_upsampled = np.interp(np.arange(0, vr_vec.size, vr_fps/fps),
+                                 np.arange(vr_vec.size),
+                                 vr_vec)
+    mocap_vec_upsampled = np.interp(np.arange(0, mocap_vec.size, mocap_fps/fps),
+                                    np.arange(mocap_vec.size),
+                                    mocap_vec)
+
+    predicted_offset = get_temporal_offset(mocap_vec_upsampled, vr_vec_upsampled, fps, 0, 10)
+    print("Predicted Offset:", predicted_offset)
+
+    #"""
+    start, end, window, gap = 0, 60, 1, 30
+    offsets = []
+    while end < vr_vec.size / vr_fps:
+        offset = get_temporal_offset(mocap_vec_upsampled, vr_vec_upsampled, fps,
+                                     predicted_offset, 10,
+                                     start=start, end=end)
+        offsets.append(offset)
+        start, end = start + gap, end + gap
+
+    variability = np.array(offsets) - predicted_offset
+    print("Variability:", variability)
+    #"""
+
+    plt.plot(np.arange(mocap_vec.size) / mocap_fps + predicted_offset, mocap_vec, 'b')
+    plt.plot(np.arange(vr_vec.size) / vr_fps, vr_vec, 'r')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
